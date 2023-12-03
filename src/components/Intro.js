dcf2c6f919a35ef6c9b19fd20a8da594b24e1d5c
luis3zc <luis3zc@gmail.com>
2023-12-03 19:13:28 +0100

    Add icon

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,7 @@ import React from "react";
 function Intro() {
     return (
         <div className="Intro">
-            <h1>Luis Corujo test workflow</h1>
+            <h1>Luis Corujo</h1>
             <h4>Software Engineer</h4>
             <p style={{maxWidth: "42ch"}}>
                 Passionate about engineering and science, driven by curiosity, and always excited to tackle hard problems.
